9caa100f146bcb648154059477fdfe64dcd366c1
Jadon Co <102571575+JadonCo101@users.noreply.github.com>
2024-02-29 10:05:44 -0500

    Add files via upload

---
+++ CS_2110/charmander.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=50x37 charmander charmander.png 
+ * Time-stamp: Tuesday 11/08/2022, 02:41:46
+ * 
+ * Image Information
+ * -----------------
+ * charmander.png 50@37
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef CHARMANDER_H
+#define CHARMANDER_H
+
+extern const unsigned short charmander[1850];
+#define CHARMANDER_SIZE 3700
+#define CHARMANDER_LENGTH 1850
+#define CHARMANDER_WIDTH 50
+#define CHARMANDER_HEIGHT 37
+
+#endif
+
